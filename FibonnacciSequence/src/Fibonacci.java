a9dfc4badf64ec73acaa9c13385052569efe5d07
youatik <y_ouatik@hotmail.com>
2023-06-01 19:29:23 -0400

    Ajout de javadocs sur Fibonacci.java

--- FibonnacciSequence/src/Fibonacci.java
+++ FibonnacciSequence/src/Fibonacci.java
@@ -1,4 +1,14 @@
+/**
+ * Cette classe fournit une méthode pour calculer le nième nombre de Fibonacci.
+ */
 public class Fibonacci {
+
+    /**
+     * Calcule le nième nombre de Fibonacci.
+     *
+     * @param n La position du nombre de Fibonacci à calculer.
+     * @return Le nième nombre de Fibonacci.
+     */
     public static int fibonacci(int n) {
         if (n <= 1)
             return n;
