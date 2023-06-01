a81723366bce89a14cfe415bf6050ab4f885e4c5
youatik <y_ouatik@hotmail.com>
2023-06-01 19:40:29 -0400

    Ajout de javadocs sur FibonacciRecursive.java

--- FibonnacciSequence/src/FibonacciRecursive.java
+++ FibonnacciSequence/src/FibonacciRecursive.java
@@ -1,10 +1,26 @@
+/**
+ * Cette classe fournit des méthodes récursives pour calculer le nième nombre de Fibonacci.
+ */
 public class FibonacciRecursive {
+
+    /**
+     * Calcule le nième nombre de Fibonacci de manière récursive.
+     *
+     * @param n La position du nombre de Fibonacci à calculer.
+     * @return Le nième nombre de Fibonacci.
+     */
     public static int fibonacci(int n) {
         if (n <= 1)
             return n;
         return fibonacciRecursive(n);
     }
 
+    /**
+     * Calcule le nième nombre de Fibonacci de manière récursive.
+     *
+     * @param n La position du nombre de Fibonacci à calculer.
+     * @return Le nième nombre de Fibonacci.
+     */
     public static int fibonacciRecursive(int n) {
         if (n <= 1)
             return n;
