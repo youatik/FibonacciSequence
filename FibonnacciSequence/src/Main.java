b823dd374feedc2f08e3d4d6b6618c2268002af6
youatik <y_ouatik@hotmail.com>
2023-06-02 12:05:41 -0400

    Ajouté un loop dans le main sans GUI pour prendre plusieurs input.

--- FibonnacciSequence/src/Main.java
+++ FibonnacciSequence/src/Main.java
@@ -11,27 +11,35 @@ public class Main {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrez la valeur de n : ");
-        int n = scanner.nextInt();
-        //scanner.close();
+        boolean exit = false;
 
-        System.out.print("Choisissez la version de Fibonacci (1 pour itérative, 2 pour récursive) : ");
-        int choix = scanner.nextInt();
+        while (!exit) {
+            System.out.print("Entrez la valeur de n (ou -1 pour quitter) : ");
+            int n = scanner.nextInt();
+            if (n == -1) {
+                exit = true;
+                continue;
+            }
 
-        FibonacciInterface fibonacciMethod;
-        if (choix == 1) {
-            fibonacciMethod = new Fibonacci();
-        } else if (choix == 2) {
-            fibonacciMethod = new FibonacciRecursive();
-        } else {
-            System.out.println("Choix invalide. Veuillez choisir 1 ou 2.");
-            return;
+            System.out.print("Choisissez la version de Fibonacci (1 pour itérative, 2 pour récursive) : ");
+            int choix = scanner.nextInt();
+
+            FibonacciInterface fibonacciMethod;
+            if (choix == 1) {
+                fibonacciMethod = new Fibonacci();
+            } else if (choix == 2) {
+                fibonacciMethod = new FibonacciRecursive();
+            } else {
+                System.out.println("Choix invalide. Veuillez choisir 1 ou 2.");
+                continue;
+            }
+
+            FibonacciInterface timedFibonacci = new FibonacciTimingDecorator(fibonacciMethod);
+            int resultat = timedFibonacci.fibonacci(n);
+
+            System.out.println("Le " + n + "ème nombre de Fibonacci est : " + resultat);
         }
 
-        FibonacciInterface timedFibonacci = new FibonacciTimingDecorator(fibonacciMethod);
-        int resultat = timedFibonacci.fibonacci(n);
-
-        System.out.println("Le " + n + "ème nombre de Fibonacci est : " + resultat);
+        System.out.println("Programme terminé.");
     }
 }
-
