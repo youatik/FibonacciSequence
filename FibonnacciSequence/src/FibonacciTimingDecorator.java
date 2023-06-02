0d83b7c55a82c2c8510700f8d4751fe16cbe1cd1
youatik <y_ouatik@hotmail.com>
2023-06-02 12:19:46 -0400

    Modifié le TimingDecorator pour formatter le temps, mais un nombre de fibonacci plus grand que n=47 cause un  "integer overflow"

--- FibonnacciSequence/src/FibonacciTimingDecorator.java
+++ FibonnacciSequence/src/FibonacciTimingDecorator.java
@@ -11,8 +11,20 @@ public class FibonacciTimingDecorator implements FibonacciInterface {
         int result = fibonacciMethod.fibonacci(n);
         long endTime = System.nanoTime();
         long executionTime = endTime - startTime;
-        System.out.println("Execution time: " + executionTime + " nanoseconds");
+        String formattedTime = formatExecutionTime(executionTime);
+        System.out.println("Execution time: " + formattedTime);
         return result;
     }
-}
 
+    private String formatExecutionTime(long executionTime) {
+        if (executionTime < 1_000) {
+            return executionTime + " nanoseconds";
+        } else if (executionTime < 1_000_000) {
+            return (executionTime / 1_000) + " microseconds";
+        } else if (executionTime < 1_000_000_000) {
+            return (executionTime / 1_000_000) + " milliseconds";
+        } else {
+            return (executionTime / 1_000_000_000) + " seconds";
+        }
+    }
+}
