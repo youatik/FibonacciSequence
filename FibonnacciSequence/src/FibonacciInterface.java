9a1b4d2ed833429254a2ad3bd8ee9287b200f408
youatik <y_ouatik@hotmail.com>
2023-06-02 11:25:17 -0400

    Ajout de javadocs sur FibonacciRecursive.java

---
+++ FibonnacciSequence/src/FibonacciInterface.java
@@ -0,0 +1,10 @@
+public interface FibonacciInterface {
+    /**
+     * Calculates the nth Fibonacci number.
+     *
+     * @param n The position of the Fibonacci number to calculate.
+     * @return The nth Fibonacci number.
+     */
+    int fibonacci(int n);
+}
+
