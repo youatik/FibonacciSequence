3dae84948d6b46a01a28652fd8a5fb7bf34b8fd3
youatik <112843400+youatik@users.noreply.github.com>
2023-06-02 21:24:17 -0400

    Ce commit a (1) renomme le Main Main.java

--- FibonnacciSequence/src/Main.java
+++ FibonnacciSequence/src/Main.java
@@ -1,32 +1,21 @@
 import java.util.Scanner;
 
-/**
- * Classe principale contenant la méthode main.
- */
 public class Main {
-    /**
-     * Point d'entrée du programme.
-     *
-     * @param args Les arguments de ligne de commande.
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean exit = false;
 
-        while (!exit) {
+        while (true) {
             System.out.print("Entrez la valeur de n (ou -1 pour quitter) : ");
             int n = scanner.nextInt();
             if (n == -1) {
-                exit = true;
-                continue;
+                break;
             }
 
             int resultat = new FibonacciTimingDecorator(new Fibonacci()).fibonacci(n);
             int resultatRecursive = new FibonacciTimingDecorator(new FibonacciRecursive()).fibonacci(n);
 
             System.out.println("Le " + n + "ème nombre de Fibonacci est : " + resultat);
-            System.out.println("Le " + n + "ème nombre de Fibonacci est : " + resultatRecursive);
-
+            System.out.println("Le " + n + "ème nombre de Fibonacci Récursif est : " + resultatRecursive);
         }
 
         System.out.println("Programme terminé.");
