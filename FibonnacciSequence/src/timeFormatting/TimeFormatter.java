df29660ebef46516311e9f00e3c2a5e9a8ca5d80
youatik <112843400+youatik@users.noreply.github.com>
2023-06-03 11:18:29 -0400

    Simplification subséquente de la classe TimeFormatter.java avec la méthode "%.2f"

--- FibonnacciSequence/src/timeFormatting/TimeFormatter.java
+++ FibonnacciSequence/src/timeFormatting/TimeFormatter.java
@@ -1,24 +1,21 @@
 package timeFormatting;
 
-import java.text.DecimalFormat;
 
 public class TimeFormatter {
     public String formatExecutionTime(long executionTime) {
         if (executionTime < 1_000) {
-            return formatWithDecimal(executionTime, 2) + " nanoseconds";
+            return formatWithDecimal(executionTime) + " nanoseconds";
         } else if (executionTime < 1_000_000) {
-            return formatWithDecimal((double) executionTime / 1_000, 2) + " microseconds";
+            return formatWithDecimal((double) executionTime / 1_000) + " microseconds";
         } else if (executionTime < 1_000_000_000) {
-            return formatWithDecimal((double) executionTime / 1_000_000, 2) + " milliseconds";
+            return formatWithDecimal((double) executionTime / 1_000_000) + " milliseconds";
         } else {
-            return formatWithDecimal((double) executionTime / 1_000_000_000, 2) + " seconds";
+            return formatWithDecimal((double) executionTime / 1_000_000_000) + " seconds";
         }
     }
 
-    private String formatWithDecimal(double value, int minFractionDigits) {
-        DecimalFormat decimalFormat = new DecimalFormat("#.00");
-        decimalFormat.setMinimumFractionDigits(minFractionDigits);
-        decimalFormat.setMaximumFractionDigits(minFractionDigits);
-        return decimalFormat.format(value);
+    private String formatWithDecimal(double value) {
+        return String.format("%.2f", value);
     }
+
 }
