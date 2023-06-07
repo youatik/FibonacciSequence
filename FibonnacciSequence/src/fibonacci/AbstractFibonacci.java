e0767797bd4bafbe7ca006cf902f57a42e7a0c60
Tim <roppa070@gmail.com>
2023-06-07 08:15:47 -0400

    Commentaires

--- FibonnacciSequence/src/fibonacci/AbstractFibonacci.java
+++ FibonnacciSequence/src/fibonacci/AbstractFibonacci.java
@@ -4,23 +4,22 @@ import java.util.concurrent.CountDownLatch;
 
 // Cette classe est une classe abstraite représentant une implémentation de base du calcul de la suite de Fibonacci.
 public abstract class AbstractFibonacci implements Runnable{
+
     // Le n-ième nombre dans la suite de Fibonacci à calculer.
     protected final int n;
+
     // Objet CountDownLatch utilisé pour la synchronisation dans un contexte multithread.
     protected final CountDownLatch synchro;
 
-    // Le temps de calcul du n-ième nombre de Fibonacci.
     protected long resultTime;
-    // Le tableau qui contiendra les nombres de la suite de Fibonacci.
+
     protected long[] display;
 
-    // Le constructeur qui initialise le n-ième nombre à calculer et l'objet de synchronisation.
     public AbstractFibonacci(int n, CountDownLatch synchro) {
         this.n = n;
         this.synchro = synchro;
     }
 
-    // Cette méthode doit être implémentée dans une sous-classe.
     public abstract void run();
 
     // Cette méthode retourne le temps de calcul du n-ième nombre de Fibonacci.
@@ -32,7 +31,6 @@ public abstract class AbstractFibonacci implements Runnable{
     public long[] getDisplay() {
         return display;
     }
-
 
     // Cette méthode doit être implémentée dans une sous-classe pour calculer la suite de Fibonacci.
     protected abstract long[] fib(int n);
